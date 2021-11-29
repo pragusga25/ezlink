574c2402069b999ac47b074adf16e88d50c3f310
Taufik Pragusga <pragusga25@gmail.com>
2021-11-29 20:24:50 +0700

    fix(redirect url): add https prefix to url that doesnt have https prefix

--- app/pages/[code].tsx
+++ app/pages/[code].tsx
@@ -8,9 +8,18 @@ const RedirectPage: BlitzPage = () => {
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const code = context.query.code as string
   const data = await db.url.findUnique({ where: { code } })
+  let redirectUrl = "/"
+  if (data) {
+    if (!data.url.startsWith("http://") && !data.url.startsWith("https://")) {
+      redirectUrl = "https://" + data.url
+    } else {
+      redirectUrl = data.url
+    }
+  }
+
   return {
     redirect: {
-      destination: data?.url ?? "/",
+      destination: redirectUrl,
       permanent: false,
     },
   }
