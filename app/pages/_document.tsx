5f80d388d6d91bfc11eb4029918cb8d2642410f6
Taufik Pragusga <pragusga25@gmail.com>
2021-11-29 13:34:16 +0700

    feat(seo): add meta tags

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -20,6 +20,22 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <DocumentHead>
+          <meta name="description" content="A simple and free url shortener" />
+          <meta name="keywords" content="shortener, short, url, short url" />
+          <meta name="author" content="Pragusga" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+          <meta property="og:title" content="GWJ.PW | URL Shortener" />
+          <meta property="og:description" content="A simple and free url shortener" />
+          <meta property="og:url" content="https://www.gwj.pw" />
+          <meta property="og:site_name" content="GWJ.PW" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="https://www.gwj.pw" />
+          <meta name="twitter:creator" content="@pragusga" />
+          <meta name="twitter:title" content="GWJ.PW" />
+          <meta name="twitter:description" content="A simple and free url shortener" />
+
           <link href="https://fonts.googleapis.com/css2?family=Inter" rel="stylesheet" />
         </DocumentHead>
         <body>
